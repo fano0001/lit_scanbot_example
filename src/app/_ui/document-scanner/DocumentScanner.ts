aa953157de325d51e3d7a19c2c95f5e0c35bb4ab
Fabian Nowak <fabian.nowak@axa.de>
2024-10-31 13:54:31 +0100

    refactor

--- src/app/_ui/document-scanner/DocumentScanner.ts
+++ src/app/_ui/document-scanner/DocumentScanner.ts
@@ -17,6 +17,10 @@ export class DocumentScanner extends LitElement {
 
     scanner?: HTMLElement
 
+
+    // to not use the ShadowRoot feature from Lit, enable the following method
+    // if enabled the error of ScanBot will appear
+
     // override createRenderRoot() {
     //     return this
     // }
@@ -34,62 +38,19 @@ export class DocumentScanner extends LitElement {
     private async initializeScanner () {
         const scanbotSDK: ScanbotSDK = await ScanbotSDK.initialize({licenseKey: ''})
 
-        const iDocumentScannerHandle = await scanbotSDK.createDocumentScanner({
+        await scanbotSDK.createDocumentScanner({
             container: this.documentScannerContainer,
             onDocumentDetected: this.onDocumentDetected,
             autoCaptureEnabled: false,
-            onError: this.onError,
             style : {
                 captureButton: {
                     color: "blue"
-                },
-                outline: {
-                    polygon: {
-                        strokeCapturing: "green",
-                        strokeSearching: "yellow",
-                        fillCapturing: "transparent",
-                        fillSearching: "transparent",
-                        strokeWidth: "2px"
-                    },
-                    label: {
-                        position: "absolute",
-                        top: "90%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        textAlign: "center",
-                        backgroundColor: "rgba(0, 0, 0, 0.7)",
-                        color: "white",
-                        borderRadius: "0.25em",
-                        padding: "0.5em",
-                        fontFamily: "sans-serif",
-                        fontSize: "1em"
-                    },
-                    path: {
-                        stroke: "green",
-                        strokeWidth: 4
-                    }
-                },
-            },
-            text: {
-                hint: {
-                    OK: 'Capturing your document... Please do not move the camera.',
-                    OK_SmallSize: 'The document is too small. Try moving closer.',
-                    OK_BadAngles: 'This is a bad camera angle. Hold the device straight over the document.',
-                    OK_BadAspectRatio: 'Rotate the device sideways, so that the document fits better into the screen.',
-                    OK_OffCenter: 'Try holding the device at the center of the document.',
-                    Error_NothingDetected: 'Bitte halte dein Kamera über das Dokument um mit dem Scannen zu beginnen!',
-                    Error_Brightness: 'It is too dark. Try turning on a light.',
-                    Error_Noise: 'Please move the document to a clear surface.'
                 }
             }
         })
     }
 
-    private onError(e: any) : void{
-        console.error('Kaputt', e)
-    }
-
-    private onDocumentDetected(scan: any): void {
-        console.log('Anzahl scans: ')
+    private onDocumentDetected(): void {
+        console.log('scan triggert')
     }
 }
